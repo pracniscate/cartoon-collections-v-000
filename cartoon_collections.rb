27afd07c3ecfcf7ec0569b2ff8d643e1e47e856d
Lorena <lorena.giddens@outlook.com>
2018-06-06 14:20:34 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.any? {|i| i > 4}
+  calls.any? {|call| call.size > 4}
 end
 
 def find_the_cheese# code an argument here
